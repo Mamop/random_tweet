729d86dd524494339b9801e57dc69de24cb0b51f
Mamop <38277992+Mamop@users.noreply.github.com>
2019-03-20 14:56:24 +0900

    Update random_tweet.py

--- random_tweet.py
+++ random_tweet.py
@@ -1,15 +1,21 @@
 import tweepy
 import random
+import os
 
-CONSUMER_KEY="xxx"
-CONSUMER_SECRET="xxx"
+CONSUMER_KEY=os.environ.get("Consumer_key")
+CONSUMER_SECRET=os.environ.get("Consumer_secret")
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-ACCESS_TOKEN='xxx'
-ACCESS_SECRET='xxx'
+ACCESS_TOKEN=os.environ.get("Access_token_key")
+ACCESS_SECRET=os.environ.get("Access_token_secret")
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 
 api=tweepy.API(auth)
 
-STATUS_DATA=["abc","bcd",
-             "cde","def"]
+abc=os.environ.get("text1")
+bcd=os.environ.get("text2")
+cde=os.environ.get("text3")
+def=os.environ.get("text4")
+
+STATUS_DATA=[abc,bcd,
+             cde,def]
 api.update_status(random.choice(STATUS_DATA))
